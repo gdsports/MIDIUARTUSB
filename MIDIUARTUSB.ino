03223a64e56b56791204d10c8d7a43a9d6d0e3f7
gdsports <gdsports625@gmail.com>
2018-03-05 12:17:51 -1000

    Big clean up
    Simplify the code. Add test script.

--- MIDIUARTUSB.ino
+++ MIDIUARTUSB.ino
@@ -38,85 +38,11 @@ SOFTWARE.
 struct MySettings : public midi::DefaultSettings
 {
     static const bool Use1ByteParsing = false;
-    static const unsigned SysExMaxSize = 1024; // Accept SysEx messages up to 1024 bytes long.
+    static const unsigned SysExMaxSize = 1026; // Accept SysEx messages up to 1024 bytes long.
     static const long BaudRate = 31250;
 };
 
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI, MySettings);
-
-/*************** MIDI USB functions ****************************/
-
-// First parameter is the event type (0x09 = note on, 0x08 = note off).
-// Second parameter is note-on/note-off, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the note number (48 = middle C).
-// Fourth parameter is the velocity (64 = normal, 127 = fastest).
-
-void USBNoteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t evt = {0x08, (byte)(0x80 | (channel - 1)), pitch, velocity};
-  MidiUSB.sendMIDI(evt);
-}
-
-void USBNoteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t evt = {0x09, (byte)(0x90 | (channel - 1)), pitch, velocity};
-  MidiUSB.sendMIDI(evt);
-}
-
-// First parameter is the event type (0x0A = polyphonic key pressure)
-// Second parameter is poly-keypress, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the note number (48 = middle C).
-// Fourth parameter is the pressure on the key after it "bottoms out"
-
-void USBAfterTouchPoly(byte channel, byte pitch, byte pressure) {
-  midiEventPacket_t evt = {0x0A, (byte)(0xA0 | (channel - 1)), pitch, pressure};
-  MidiUSB.sendMIDI(evt);
-}
-
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
-
-void USBControlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = {0x0B, (byte)(0xB0 | (channel - 1)), control, value};
-  MidiUSB.sendMIDI(event);
-}
-
-// First parameter is the event type (0x0C = program change)
-// Second parameter is program number, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the program number.
-// Fourth parameter is 0.
-
-void USBProgramChange(byte channel, byte number) {
-  midiEventPacket_t evt = {0x0C, (byte)(0xC0 | (channel - 1)), number, 0};
-  MidiUSB.sendMIDI(evt);
-}
-
-// First parameter is the event type (0x0D = after touch channel pressure)
-// Second parameter is channel pressure, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the channel pressure value.
-// Fourth parameter is 0.
-
-void USBAfterTouchChannel(byte channel, byte pressure) {
-  midiEventPacket_t evt = {0x0D, (byte)(0xD0 | (channel - 1)), pressure, 0};
-  MidiUSB.sendMIDI(evt);
-}
-
-// First parameter is the event type (0x0E = pitch bend)
-// Second parameter is pitch bend, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the least significant 7 bits of pitch bend
-// Fourth parameter is the most significant 7 bits of pitch bend
-
-void USBPitchBend(byte channel, int bend) {
-  uint16_t uint_bend = (uint16_t)(bend - MIDI_PITCHBEND_MIN);
-  midiEventPacket_t evt = {0x0E, (byte)(0xE0 | (channel - 1)),
-    (byte)(uint_bend & 0x7F), (byte)((uint_bend >> 7) & 0x7F)};
-  MidiUSB.sendMIDI(evt);
-}
 
 void USBSystemExclusive(unsigned size, byte *data, bool markersIncluded) {
   uint8_t bytesOut;
@@ -148,244 +74,86 @@ void USBSystemExclusive(unsigned size, byte *data, bool markersIncluded) {
   }
 }
 
-void USBTimeCodeQuarterFrame(byte data)
-{
-  midiEventPacket_t evt = {0x02, midi::TimeCodeQuarterFrame, data, 0};
-  MidiUSB.sendMIDI(evt);
-}
-
-void USBSongSelect(byte songnumber)
-{
-  midiEventPacket_t evt = {0x02, midi::SongSelect, songnumber, 0};
-  MidiUSB.sendMIDI(evt);
-}
-
-void USBSongPosition(unsigned beats)
-{
-  midiEventPacket_t evt = {0x03, midi::SongPosition, (byte)(beats & 0x7F), 
-    (byte)((beats >> 7) & 0x7F)};
-  MidiUSB.sendMIDI(evt);
-}
-
-inline void USBRealTime(midi::MidiType midiType)
-{
-  midiEventPacket_t evt = {0x0F, (byte)midiType, 0, 0};
-  MidiUSB.sendMIDI(evt);
-}
-
-void USBTuneRequest(void)
-{
-  USBRealTime(midi::TuneRequest);
-}
-
-void USBTimingClock(void)
-{
-  USBRealTime(midi::Clock);
-}
-
-void USBStart(void)
-{
-  USBRealTime(midi::Start);
-}
-
-void USBContinue(void)
-{
-  USBRealTime(midi::Continue);
-}
-
-void USBStop(void)
-{
-  USBRealTime(midi::Stop);
-}
-
-void USBActiveSensing(void)
-{
-  USBRealTime(midi::ActiveSensing);
-}
-
-void USBSystemReset(void)
-{
-  USBRealTime(midi::SystemReset);
-}
-
-/*************** MIDI UART callback functions ****************************/
-
-void UARThandleNoteOn(byte inChannel, byte inNumber, byte inVelocity)
-{
-  DBGSERIAL.print(F("NoteOn  note: "));
-  DBGSERIAL.print(inNumber);
-  DBGSERIAL.print(F("\tvelocity: "));
-  DBGSERIAL.print(inVelocity);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(inChannel);
-  USBNoteOn(inChannel, inNumber, inVelocity);
-}
-void UARThandleNoteOff(byte inChannel, byte inNumber, byte inVelocity)
-{
-  DBGSERIAL.print(F("NoteOff note: "));
-  DBGSERIAL.print(inNumber);
-  DBGSERIAL.print(F("\tvelocity: "));
-  DBGSERIAL.print(inVelocity);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(inChannel);
-  USBNoteOff(inChannel, inNumber, inVelocity);
-}
-
-void UARThandleAfterTouchPoly(byte channel, byte note, byte pressure)
-{
-  DBGSERIAL.print(F("AfterTouchPoly note: "));
-  DBGSERIAL.print(note);
-  DBGSERIAL.print(F("\tpressure: "));
-  DBGSERIAL.print(pressure);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(channel);
-  USBAfterTouchPoly(channel, note, pressure);
-}
-
-void UARThandleControlChange(byte channel, byte number, byte value)
-{
-  DBGSERIAL.print(F("ControlChange number: "));
-  DBGSERIAL.print(number);
-  DBGSERIAL.print(F("\tvalue: "));
-  DBGSERIAL.print(value);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(channel);
-  USBControlChange(channel, number, value);
-}
-
-void UARThandleProgramChange(byte channel, byte number)
-{
-  DBGSERIAL.print(F("ProgramChange number: "));
-  DBGSERIAL.print(number);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(channel);
-  USBProgramChange(channel, number);
-}
-
-void UARThandleAfterTouchChannel(byte channel, byte pressure)
-{
-  DBGSERIAL.print(F("AfterTouchChannel pressure: "));
-  DBGSERIAL.print(pressure);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(channel);
-  USBAfterTouchChannel(channel, pressure);
-}
-
-void UARThandlePitchBend(byte channel, int bend)
-{
-  DBGSERIAL.print(F("PitchBend bend: "));
-  DBGSERIAL.print(bend & 0x3FFF, HEX);
-  DBGSERIAL.print(F("\tchannel: "));
-  DBGSERIAL.println(channel);
-  USBPitchBend(channel, bend);
-}
-
-void UARThandleSystemExclusive(byte* array, unsigned size)
-{
-  DBGSERIAL.print(F("SystemExclusive size: "));
-  DBGSERIAL.println(size);
-  USBSystemExclusive(size, array, true);
-}
-
-void UARThandleTimeCodeQuarterFrame(byte data)
-{
-  DBGSERIAL.print(F("TimeCodeQuarterFrame data: "));
-  DBGSERIAL.println(data);
-  //MIDI.sendTimeCodeQuarterFrame((data & 0x70) >> 4, data & 0x0F);
-}
-
-void UARThandleSongPosition(unsigned beats)
-{
-  DBGSERIAL.print(F("SongPosition beats: "));
-  DBGSERIAL.println(beats);
-  USBSongPosition(beats);
-}
-
-void UARThandleSongSelect(byte songnumber)
-{
-  DBGSERIAL.print(F("SongSelect number: "));
-  DBGSERIAL.println(songnumber);
-  USBSongSelect(songnumber);
-}
-
-void UARThandleTuneRequest(void)
-{
-  DBGSERIAL.println(F("TuneRequest"));
-  USBTuneRequest();
-}
-
-void UARThandleClock(void)
-{
-  DBGSERIAL.println(F("Clock"));
-  USBTimingClock();
-}
-
-void UARThandleStart(void)
-{
-  DBGSERIAL.println(F("Start"));
-  USBStart();
-}
-
-void UARThandleContinue(void)
-{
-  DBGSERIAL.println(F("Continue"));
-  USBContinue();
-}
-
-void UARThandleStop(void)
-{
-  DBGSERIAL.println(F("Stop"));
-  USBStop();
-}
-
-void UARThandleActiveSensing(void)
-{
-  DBGSERIAL.println(F("ActiveSensing"));
-  USBActiveSensing();
-}
-
-void UARThandleSystemReset(void)
-{
-  DBGSERIAL.println(F("SystemReset"));
-  USBSystemReset();
-}
-
 void setup() {
   DBGSERIAL.begin(115200);
 
-  MIDI.begin();
+  MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.turnThruOff();
-  MIDI.setHandleNoteOn(UARThandleNoteOn);
-  MIDI.setHandleNoteOff(UARThandleNoteOff);
-  MIDI.setHandleAfterTouchPoly(UARThandleAfterTouchPoly);
-  MIDI.setHandleControlChange(UARThandleControlChange);
-  MIDI.setHandleProgramChange(UARThandleProgramChange);
-  MIDI.setHandleAfterTouchChannel(UARThandleAfterTouchChannel);
-  MIDI.setHandlePitchBend(UARThandlePitchBend);
-  MIDI.setHandleSystemExclusive(UARThandleSystemExclusive);
-  MIDI.setHandleTimeCodeQuarterFrame(UARThandleTimeCodeQuarterFrame);
-  MIDI.setHandleSongPosition(UARThandleSongPosition);
-  MIDI.setHandleSongSelect(UARThandleSongSelect);
-  MIDI.setHandleTuneRequest(UARThandleTuneRequest);
-  MIDI.setHandleClock(UARThandleClock);
-  MIDI.setHandleStart(UARThandleStart);
-  MIDI.setHandleContinue(UARThandleContinue);
-  MIDI.setHandleStop(UARThandleStop);
-  MIDI.setHandleActiveSensing(UARThandleActiveSensing);
-  MIDI.setHandleSystemReset(UARThandleSystemReset);
 }
 
 uint32_t sysexSize = 0;
 
 void loop()
 {
-  // MIDI UART -> MIDI USB
-  MIDI.read();
-  MidiUSB.flush();
+  /* MIDI UART -> MIDI USB */
+  if (MIDI.read()) {
+    midi::MidiType msgType = MIDI.getType();
+    DBGSERIAL.print(msgType, HEX);
+    DBGSERIAL.print(' ');
+    DBGSERIAL.print(MIDI.getData1(), HEX);
+    DBGSERIAL.print(' ');
+    DBGSERIAL.println(MIDI.getData2(), HEX);
+    switch (msgType) {
+      case midi::InvalidType:
+        break;
+      case midi::NoteOff:
+      case midi::NoteOn:
+      case midi::AfterTouchPoly:
+      case midi::ControlChange:
+      case midi::ProgramChange:
+      case midi::AfterTouchChannel:
+      case midi::PitchBend:
+        {
+          midiEventPacket_t tx = {
+            (byte)(msgType>>4),
+            (byte)((msgType & 0xF0) | ((MIDI.getChannel()-1) & 0x0F)), /* getChannel() returns values from 1 to 16 */
+            MIDI.getData1(),
+            MIDI.getData2()
+          };
+          MidiUSB.sendMIDI(tx);
+          MidiUSB.flush();
+          break;
+        }
+      case midi::SystemExclusive:
+        USBSystemExclusive(MIDI.getSysExArrayLength(),
+            (byte *)MIDI.getSysExArray(), true);
+        MidiUSB.flush();
+        break;
+      case midi::TuneRequest:
+      case midi::Clock:
+      case midi::Start:
+      case midi::Continue:
+      case midi::Stop:
+      case midi::ActiveSensing:
+      case midi::SystemReset:
+        {
+          midiEventPacket_t tx = { 0x0F, (byte)(msgType), 0, 0 };
+          MidiUSB.sendMIDI(tx);
+          MidiUSB.flush();
+          break;
+        }
+      case midi::TimeCodeQuarterFrame:
+      case midi::SongSelect:
+        {
+          midiEventPacket_t tx = { 0x02, (byte)(msgType), MIDI.getData1(), 0 };
+          MidiUSB.sendMIDI(tx);
+          MidiUSB.flush();
+          break;
+        }
+      case midi::SongPosition:
+        {
+          midiEventPacket_t tx = { 0x03, (byte)(msgType), MIDI.getData1(), MIDI.getData2() };
+          MidiUSB.sendMIDI(tx);
+          MidiUSB.flush();
+          break;
+        }
+      default:
+        break;
+    }
+  }
 
-  // MIDI USB -> MIDI UART
-  midiEventPacket_t rx;
-  rx = MidiUSB.read();
+  /* MIDI USB -> MIDI UART */
+  midiEventPacket_t rx = MidiUSB.read();
   if (rx.header != 0) {
     midi::Channel channel = (rx.byte1 & 0x0F) + 1;
     midi::MidiType midiType = (midi::MidiType)(rx.byte1 & 0xF0);
--- README.md
+++ README.md
@@ -4,12 +4,39 @@ Bi-directional converter between MIDI USB and MIDI UART. There are many pages
 that describe how to connect a UART port to DIN connectors so it is not covered
 here.
 
-Maximum System Exclusive size is 1024 bytes.
+Maximum System Exclusive size is 1024 bytes. This can be bigger on SAMD boards
+which have more RAM.
+
+Install MIDI Library and MIDIUSB libraries using the Arduino IDE library manager.
 
 Tested in 32u4 boards such as Arduino Leonardo, SparkFun Pro Micro, and Adafuit
-Itsy Bitsy.
-
-Install MIDI Library and MIDIUSB libraries.
+Itsy Bitsy 32u4.
 
 Tested on Arduino Zero and Adafruit Trinket M0. It might work on other SAMD21
 boards.
+
+## Testing
+
+Tested in with hardware loop back on UART Tx/Rx using SendMIDI and ReceiveMIDI.
+
+In one command line window run receivemidi like this:
+
+$ receivemidi dev arduino
+
+In another command line window run sendmidi with the test script
+receivemidi.txt like this:
+
+$ sendmidi dev arduino file receivemidi.txt
+
+Compare the output of receivemidi (redirect output to a file) with the
+receivemidi.txt test script.
+
+## References
+
+https://www.midi.org/specifications/
+
+http://www.usb.org/developers/docs/devclass_docs/midi10.pdf
+
+https://github.com/gbevin/ReceiveMIDI
+
+https://github.com/gbevin/SendMIDI
